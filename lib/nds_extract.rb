e5a1a01dbb9a7d83ead5e6d1673a75afd147d77c
J <jmrumingan@gmail.com>
2019-11-07 15:30:19 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,9 +7,6 @@ require 'directors_database'
 
 def directors_totals(nds)
   
-  #pp nds 
-  total_value = {}
-  #
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   #
   # Use loops, variables and the accessing method, [], to loop through the NDS
@@ -19,40 +16,54 @@ def directors_totals(nds)
   # ...
   #after finding the correct line, follow the nested AoHoAoH 
   
+  total_value = {}
   director_counter = 0 
   
   while director_counter<nds.length do
     director_name = nds[director_counter][:name]
     
     total_value[director_name] = 0 #creates hash, sets to integer 
-    puts director_name
+    #puts director_name
     #prints director's name 
     
-    total_value[director_name] = gross_for_director(nds, director_counter)
+    total_value[director_name] = gross_for_director_bypass(nds, director_counter)
     
-    puts "Gross total: #{gross_for_director(nds, director_counter)}"
-    puts "**** \n"
+    #puts "Gross total: #{gross_for_director_bypass(nds, director_counter)}"
+    #puts "**** \n"
     
     director_counter += 1 
   end 
   # Be sure to return the result at the end!
-  puts total_value
+  #puts total_value
   return total_value 
 end
 
-# Find a way to accumulate the :worldwide_grosses and return that Integer
-# using director_data as input
-
-def gross_for_director(nds, director_counter)
+# same as below but specifically used with the loop counter in directors_totals
+def gross_for_director_bypass(nds, director_counter)
   gross_total = 0 
   movie_counter = 0 
       while movie_counter<nds[director_counter][:movies].length do 
       
       #print out values to check the loop works  
-      puts nds[director_counter][:movies][movie_counter][:worldwide_gross]
+      #puts nds[director_counter][:movies][movie_counter][:worldwide_gross]
       
       gross_total = gross_total + nds[director_counter][:movies][movie_counter][:worldwide_gross]
       movie_counter += 1 
     end 
   return gross_total
+end
+
+#takes a specific director hash and returns the total of gross 
+def gross_for_director(nds)
+  puts "*starting GFD"
+  puts nds 
+  gross_count = 0 
+  i = 0 
+
+  while i < nds[:movies].length do
+    
+    gross_count += nds[:movies][i][:worldwide_gross]
+    i+= 1 
+  end 
+  return gross_count 
 end
